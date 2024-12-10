4f3a58050318e69ef04c4f808fde707e0c15a688
David Deininger <53495451+deininger@users.noreply.github.com>
2024-12-09 20:43:06 -0500

    Day 9 faster

--- src/aoc/year24/Day09.java
+++ src/aoc/year24/Day09.java
@@ -3,6 +3,7 @@ package aoc.year24;
 import aoc.util.PuzzleApp;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public class Day09 extends PuzzleApp {
     public static void main(String[] args) {
@@ -20,21 +21,21 @@ public class Day09 extends PuzzleApp {
     SortedSet<FileBlock> filesystem = new TreeSet<>();
     SortedSet<FileBlock> freespace = new TreeSet<>();
 
-    SortedSet<FileBlock> filesystemPartTwo = new TreeSet<>();
-    SortedSet<FileBlock> freespacePartTwo = new TreeSet<>();
+    SortedSet<FileBlock> filesystemPartTwo = new ConcurrentSkipListSet<>();
+    SortedSet<FileBlock> freespacePartTwo = new ConcurrentSkipListSet<>();
 
     public void parseLine(String line) {
         int position = 0;
         for (int i = 0; i < line.length(); i++) {
             int length = line.charAt(i) - '0';
             if (length == 0) continue;
-            if (i%2==1) {
-                freespace.add(new FileBlock(-1,'.', position, length));
-                freespacePartTwo.add(new FileBlock(-1,'.', position, length));
+            if (i % 2 == 1) {
+                freespace.add(new FileBlock(-1, '.', position, length));
+                freespacePartTwo.add(new FileBlock(-1, '.', position, length));
             } else {
-                char symbol = symbols.charAt((i/2)%symbols.length());
-                filesystem.add(new FileBlock(i/2, symbol, position, length));
-                filesystemPartTwo.add(new FileBlock(i/2, symbol, position, length));
+                char symbol = symbols.charAt((i / 2) % symbols.length());
+                filesystem.add(new FileBlock(i / 2, symbol, position, length));
+                filesystemPartTwo.add(new FileBlock(i / 2, symbol, position, length));
             }
             position += length;
         }
@@ -54,28 +55,28 @@ public class Day09 extends PuzzleApp {
                 filesystem.remove(file); // remove the block and add them at the end to get them re-sorted.
                 freespace.remove(free);
 
-                FileBlock newFree = new FileBlock(free.id(),free.symbol(),file.start()+file.length()-moved,moved);
+                FileBlock newFree = new FileBlock(free.id(), free.symbol(), file.start() + file.length() - moved, moved);
                 freespace.add(newFree);
 
                 file.setStart(free.start());
 
-                free.setStart(free.start()+moved);
-                free.setLength(free.length()-moved);
+                free.setStart(free.start() + moved);
+                free.setLength(free.length() - moved);
 
                 filesystem.add(file);
-                if (free.length()>0) freespace.add(free);
+                if (free.length() > 0) freespace.add(free);
             } else {
                 // We can't move the whole thing, move as much as we can (split the file)
                 int moved = free.length();
                 filesystem.remove(file); // remove the block and add them at the end to get them re-sorted.
                 freespace.remove(free);
 
-                FileBlock newFile = new FileBlock(file.id(),file.symbol(),free.start(),moved);
+                FileBlock newFile = new FileBlock(file.id(), file.symbol(), free.start(), moved);
                 filesystem.add(newFile);
 
-                file.setLength(file.length()-moved); // Shrink the remaining end file
+                file.setLength(file.length() - moved); // Shrink the remaining end file
 
-                free.setStart(file.start()+file.length()); // Move the free space to the end
+                free.setStart(file.start() + file.length()); // Move the free space to the end
 
                 filesystem.add(file);
                 freespace.add(free);
@@ -92,35 +93,29 @@ public class Day09 extends PuzzleApp {
     public void processPartTwo() {
         // System.out.println(printFilesystem(filesystemPartTwo, freespacePartTwo));
 
-        boolean makingProgress = true;
-        while (makingProgress) {
-            makingProgress = false;
+        for (FileBlock file : filesystemPartTwo.reversed()) {
+            Optional<FileBlock> oFree = freespacePartTwo.stream()
+                    .filter(free -> free.start() < file.start() && free.length() >= file.length())
+                    .findFirst();
 
-            for (FileBlock free: freespacePartTwo) {
-                Optional<FileBlock> oFile = filesystemPartTwo.reversed().stream().filter(f -> f.start() > free.start() && f.length() <= free.length()).findFirst();
-                if (oFile.isPresent()) {
-                    FileBlock file = oFile.get();
-                    // We have found a file that fits, move it:
+            if (oFree.isPresent()) {
+                FileBlock free = oFree.get();
+                // We have found a file that fits, move it:
 
-                    int moved = file.length();
-                    filesystemPartTwo.remove(file); // remove the block and add them at the end to get them re-sorted.
-                    freespacePartTwo.remove(free);
+                int moved = file.length();
+                filesystemPartTwo.remove(file); // remove the block and add them at the end to get them re-sorted.
+                freespacePartTwo.remove(free);
 
-                    FileBlock newFree = new FileBlock(free.id(),free.symbol(),file.start()+file.length()-moved,moved);
-                    freespacePartTwo.add(newFree);
+                FileBlock newFree = new FileBlock(free.id(), free.symbol(), file.start() + file.length() - moved, moved);
+                freespacePartTwo.add(newFree);
 
-                    file.setStart(free.start());
+                file.setStart(free.start());
 
-                    free.setStart(free.start()+moved);
-                    free.setLength(free.length()-moved);
+                free.setStart(free.start() + moved);
+                free.setLength(free.length() - moved);
 
-
-                    filesystemPartTwo.add(file);
-                    if (free.length()>0) freespacePartTwo.add(free);
-
-                    makingProgress = true;
-                    break;
-                }
+                filesystemPartTwo.add(file);
+                if (free.length() > 0) freespacePartTwo.add(free);
             }
         }
     }
@@ -139,7 +134,7 @@ public class Day09 extends PuzzleApp {
         SortedSet<FileBlock> s = new TreeSet<>(fs1);
         s.addAll(fs2);
 
-        for (FileBlock fb: s) {
+        for (FileBlock fb : s) {
             for (int i = 0; i < fb.length(); i++) {
                 sb.append(fb.symbol());
             }
@@ -198,13 +193,12 @@ public class Day09 extends PuzzleApp {
         }
 
         public String toString() {
-            return id + " [" + symbol + "]: (" + start + "-" + (start+length-1) + ")";
+            return id + " [" + symbol + "]: (" + start + "-" + (start + length - 1) + ")";
         }
 
         @Override
         public int compareTo(FileBlock o) {
             return Integer.compare(start, o.start);
         }
-
     }
 }
