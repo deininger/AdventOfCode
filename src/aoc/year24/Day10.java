0ddd75d7133ed3c34a1bcc17a709f940a73e4d20
David Deininger <53495451+deininger@users.noreply.github.com>
2024-12-10 10:37:34 -0500

    Day 10 WIP

--- src/aoc/year24/Day10.java
+++ src/aoc/year24/Day10.java
@@ -6,6 +6,8 @@ import aoc.util.PuzzleApp;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day10 extends PuzzleApp {
     public static void main(String[] args) {
@@ -25,16 +27,18 @@ public class Day10 extends PuzzleApp {
     }
 
     private Set<Loc> step(Set<Loc> starts, char next) {
-        Set<Loc> newLocs = new HashSet<>();
+        Set<Loc> newLocs = ConcurrentHashMap.newKeySet();
 
-        for (Loc start: starts) {
+        // System.out.println("Stepping " + starts.size() + " locs to '" + next + "'");
+
+        starts.stream().forEach(start -> {
             newLocs.addAll(start.adjacent().filter(loc -> map.contains(loc)).filter(loc -> map.at(loc) == next).toList());
-        }
+        });
 
         return newLocs;
     }
 
-    private int count = 0;
+    private AtomicInteger count = new AtomicInteger(0);
 
     public void process() {
         // System.out.println(map);
@@ -42,15 +46,17 @@ public class Day10 extends PuzzleApp {
         Set<Loc> trailheads = map.locateAll('0');
         // System.out.println("0: " + trailheads);
 
-        for(Loc start: trailheads) {
+        trailheads.parallelStream().forEach(start -> {
             Set<Loc> locs = Set.of(start);
+
             for (char c = '1'; c <= '9'; c++) {
                 locs = step(locs, c);
                 // System.out.println(c + ": " + locs);
             }
-            System.out.println("Trailhead " + start + " has score " + locs.size());
-            count += locs.size();
-        }
+
+            // System.out.println("Trailhead " + start + " has score " + locs.size());
+            count.addAndGet(locs.size());
+        });
     }
 
     public void results() {
