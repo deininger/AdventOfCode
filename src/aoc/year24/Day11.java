ad1cc445c563d68d90cac63a7dcebe221e84d695
David Deininger <53495451+deininger@users.noreply.github.com>
2024-12-11 14:04:29 -0500

    Day 11

--- src/aoc/year24/Day11.java
+++ src/aoc/year24/Day11.java
@@ -2,10 +2,7 @@ package aoc.year24;
 
 import aoc.util.PuzzleApp;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Day11 extends PuzzleApp { // Day 11: Plutonian Pebbles
     public static void main(String[] args) {
@@ -18,53 +15,66 @@ public class Day11 extends PuzzleApp { // Day 11: Plutonian Pebbles
         return "data/year24/day11";
     }
 
-    private final List<Long> stones = new LinkedList<>();
+    private Map<Long,Long> stones = new HashMap<>();
 
     public void parseLine(String line) {
-        Arrays.stream(line.split("\\s+")).map(Long::parseLong).forEach(stones::add);
+        Arrays.stream(line.split("\\s+")).map(Long::parseLong).forEach(stone -> stones.merge(stone, 1L, Long::sum));
     }
 
-    private void blink() {
-        ListIterator<Long> iterator = stones.listIterator();
+    private Map<Long,Long> blink(Map<Long,Long> stones) {
+        Map<Long,Long> newStones = new HashMap<>();
+        Iterator<Map.Entry<Long,Long>> iterator = stones.entrySet().iterator();
 
         while (iterator.hasNext()) {
-            Long stone = iterator.next();
+            Map.Entry<Long,Long> entry = iterator.next();
+            Long stone = entry.getKey();
+            Long count = entry.getValue();
+            iterator.remove();
+
             if (stone == 0L) {
-                iterator.set(1L);
+                newStones.merge(1L, count, Long::sum);
             } else {
                 String stringValue = String.valueOf(stone);
                 if (stringValue.length() % 2 == 0) {
-                    iterator.set(Long.parseLong(stringValue.substring(0, stringValue.length() / 2)));
-                    iterator.add(Long.parseLong(stringValue.substring(stringValue.length() / 2)));
+                    Long firstStone = Long.parseLong(stringValue.substring(0, stringValue.length() / 2));
+                    Long secondStone = Long.parseLong(stringValue.substring(stringValue.length() / 2));
+                    newStones.merge(firstStone, count, Long::sum);
+                    newStones.merge(secondStone, count, Long::sum);
                 } else {
-                    iterator.set(stone * 2024);
+                    newStones.merge(stone * 2024, count, Long::sum);
                 }
             }
         }
+
+        return newStones;
     }
 
     public void process() {
         // System.out.println(stones);
         for (int i = 0; i < 25; i++) {
             System.out.println(i);
-            blink();
+            stones = blink(stones);
             // System.out.println(stones);
         }
     }
-
+// 2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2
     public void results() {
-        System.out.println("Day 11 part 1 result: " + stones.size());
+        long result = stones.values().stream().mapToLong(Long::longValue).sum();
+        System.out.println("Day 11 part 1 result: " + result);
     }
 
     public void processPartTwo() {
         for (int i = 25; i < 75; i++) {
             System.out.println(i);
-            blink();
+            stones = blink(stones);
         }
     }
 
     public void resultsPartTwo() {
-        System.out.println("Day 11 part 2 result: " + stones.size());
+        long result = stones.values().stream().mapToLong(Long::longValue).sum();
+        System.out.println("Day 11 part 2 result: " + result);
     }
 
+    // 8298105970 is too low
+    // 65601038650482 is too low
 }
