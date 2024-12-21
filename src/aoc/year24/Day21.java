baca973ebdf1fc5da68cbe68b5ce9ca1495575b7
David Deininger <53495451+deininger@users.noreply.github.com>
2024-12-21 17:34:37 -0500

    Day 21 WIP

--- src/aoc/year24/Day21.java
+++ src/aoc/year24/Day21.java
@@ -25,40 +25,40 @@ public class Day21 extends PuzzleApp {
         codes.add(line);
     }
 
-    private final Map<Character, Loc> numericKeypadCharacterPositions = Stream.of(new Object[][] {
-            { '7', 0, 0 },
-            { '8', 1, 0 },  // +---+---+---+
-            { '9', 2, 0 },  // | 7 | 8 | 9 |
-            { '4', 0, 1 },  // +---+---+---+
-            { '5', 1, 1 },  // | 4 | 5 | 6 |
-            { '6', 2, 1 },  // +---+---+---+
-            { '1', 0, 2 },  // | 1 | 2 | 3 |
-            { '2', 1, 2 },  // +---+---+---+
-            { '3', 2, 2 },  //     | 0 | A |
-            { '0', 1, 3 },  //     +---+---+
-            { 'A', 2, 3 }
-    }).collect(Collectors.toMap(data -> (char)data[0], data -> new Loc((int)data[1], (int)data[2])));
+    private final Map<Character, Loc> numericKeypadCharacterPositions = Stream.of(new Object[][]{
+            {'7', 0, 0},
+            {'8', 1, 0},  // +---+---+---+
+            {'9', 2, 0},  // | 7 | 8 | 9 |
+            {'4', 0, 1},  // +---+---+---+
+            {'5', 1, 1},  // | 4 | 5 | 6 |
+            {'6', 2, 1},  // +---+---+---+
+            {'1', 0, 2},  // | 1 | 2 | 3 |
+            {'2', 1, 2},  // +---+---+---+
+            {'3', 2, 2},  //     | 0 | A |
+            {'0', 1, 3},  //     +---+---+
+            {'A', 2, 3}
+    }).collect(Collectors.toMap(data -> (char) data[0], data -> new Loc((int) data[1], (int) data[2])));
 
-    private final Map<Loc,Character> invertedNumericKeypad = numericKeypadCharacterPositions.entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
-
-    private final Map<Character, Loc> directionalKeypadCharacterPositions = Stream.of(new Object[][] {
-            { '^', 1, 0 },  //     +---+---+
-            { 'A', 2, 0 },  //     | ^ | A |
-            { '<', 0, 1 },  // +---+---+---+
-            { 'v', 1, 1 },  // | < | v | > |
-            { '>', 2, 1 }   // +---+---+---+
-    }).collect(Collectors.toMap(data -> (char)data[0], data -> new Loc((int)data[1], (int)data[2])));
-
-    private final Map<Loc,Character> invertedDirectionalKeypad = directionalKeypadCharacterPositions.entrySet()
+    private final Map<Loc, Character> invertedNumericKeypad = numericKeypadCharacterPositions.entrySet()
             .stream()
             .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
 
-    private final char[] keypadPositions = { 'A', 'A', 'A' }; // index 0 is the numeric keypad, the rest are directional.
+    private final Map<Character, Loc> directionalKeypadCharacterPositions = Stream.of(new Object[][]{
+            {'^', 1, 0},  //     +---+---+
+            {'A', 2, 0},  //     | ^ | A |
+            {'<', 0, 1},  // +---+---+---+
+            {'v', 1, 1},  // | < | v | > |
+            {'>', 2, 1}   // +---+---+---+
+    }).collect(Collectors.toMap(data -> (char) data[0], data -> new Loc((int) data[1], (int) data[2])));
 
-    private static final Loc UNSAFE_NUMERIC = new Loc(0,3);
-    private static final Loc UNSAFE_DIRECTIONAL = new Loc(0,0);
+    private final Map<Loc, Character> invertedDirectionalKeypad = directionalKeypadCharacterPositions.entrySet()
+            .stream()
+            .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+
+    private final char[] keypadPositions = new char[26];
+
+    private static final Loc UNSAFE_NUMERIC = new Loc(0, 3);
+    private static final Loc UNSAFE_DIRECTIONAL = new Loc(0, 0);
 
 
     private Set<String> allMoves(Loc current, Loc target, Loc unsafe) {
@@ -159,10 +159,22 @@ public class Day21 extends PuzzleApp {
             Loc current = numericKeypadCharacterPositions.get(keypadPositions[0]);
             Loc target = numericKeypadCharacterPositions.get(c);
             Loc delta = target.difference(current);
-            while (delta.x() > 0) { delta = delta.setX(delta.x()-1); result.append( '>'); }
-            while (delta.y() < 0) { delta = delta.setY(delta.y()+1); result.append( '^'); }
-            while (delta.x() < 0) { delta = delta.setX(delta.x()+1); result.append( '<'); }
-            while (delta.y() > 0) { delta = delta.setY(delta.y()-1); result.append( 'v'); }
+            while (delta.x() > 0) {
+                delta = delta.setX(delta.x() - 1);
+                result.append('>');
+            }
+            while (delta.y() < 0) {
+                delta = delta.setY(delta.y() + 1);
+                result.append('^');
+            }
+            while (delta.x() < 0) {
+                delta = delta.setX(delta.x() + 1);
+                result.append('<');
+            }
+            while (delta.y() > 0) {
+                delta = delta.setY(delta.y() - 1);
+                result.append('v');
+            }
             result.append('A');
             keypadPositions[0] = c;
         }
@@ -176,12 +188,23 @@ public class Day21 extends PuzzleApp {
 
         for (char c : code.toCharArray()) {
             switch (c) {
-                case '<': pos = pos.move(Direction.LEFT); break;
-                case '>': pos = pos.move(Direction.RIGHT); break;
-                case '^': pos = pos.move(Direction.UP); break;
-                case 'v': pos = pos.move(Direction.DOWN); break;
-                case 'A': result.append(invertedNumericKeypad.get(pos)); break;
-                default: throw new UnsupportedOperationException("Unsupported character '" + c + "'");
+                case '<':
+                    pos = pos.move(Direction.LEFT);
+                    break;
+                case '>':
+                    pos = pos.move(Direction.RIGHT);
+                    break;
+                case '^':
+                    pos = pos.move(Direction.UP);
+                    break;
+                case 'v':
+                    pos = pos.move(Direction.DOWN);
+                    break;
+                case 'A':
+                    result.append(invertedNumericKeypad.get(pos));
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Unsupported character '" + c + "'");
             }
 
             if (!invertedNumericKeypad.containsKey(pos)) {
@@ -199,10 +222,22 @@ public class Day21 extends PuzzleApp {
             Loc current = directionalKeypadCharacterPositions.get(keypadPositions[keypadNumber]);
             Loc target = directionalKeypadCharacterPositions.get(c);
             Loc delta = target.difference(current);
-            while (delta.x() > 0) { delta = delta.setX(delta.x()-1); result.append( '>'); }
-            while (delta.y() > 0) { delta = delta.setY(delta.y()-1); result.append( 'v'); }
-            while (delta.x() < 0) { delta = delta.setX(delta.x()+1); result.append( '<'); }
-            while (delta.y() < 0) { delta = delta.setY(delta.y()+1); result.append( '^'); }
+            while (delta.x() > 0) {
+                delta = delta.setX(delta.x() - 1);
+                result.append('>');
+            }
+            while (delta.y() > 0) {
+                delta = delta.setY(delta.y() - 1);
+                result.append('v');
+            }
+            while (delta.x() < 0) {
+                delta = delta.setX(delta.x() + 1);
+                result.append('<');
+            }
+            while (delta.y() < 0) {
+                delta = delta.setY(delta.y() + 1);
+                result.append('^');
+            }
             result.append('A');
             keypadPositions[keypadNumber] = c;
         }
@@ -216,12 +251,23 @@ public class Day21 extends PuzzleApp {
 
         for (char c : code.toCharArray()) {
             switch (c) {
-                case '<': pos = pos.move(Direction.LEFT); break;
-                case '>': pos = pos.move(Direction.RIGHT); break;
-                case '^': pos = pos.move(Direction.UP); break;
-                case 'v': pos = pos.move(Direction.DOWN); break;
-                case 'A': result.append(invertedDirectionalKeypad.get(pos)); break;
-                default: throw new UnsupportedOperationException("Unsupported character '" + c + "'");
+                case '<':
+                    pos = pos.move(Direction.LEFT);
+                    break;
+                case '>':
+                    pos = pos.move(Direction.RIGHT);
+                    break;
+                case '^':
+                    pos = pos.move(Direction.UP);
+                    break;
+                case 'v':
+                    pos = pos.move(Direction.DOWN);
+                    break;
+                case 'A':
+                    result.append(invertedDirectionalKeypad.get(pos));
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Unsupported character '" + c + "'");
             }
 
             if (!invertedDirectionalKeypad.containsKey(pos)) {
@@ -235,7 +281,9 @@ public class Day21 extends PuzzleApp {
     private int partOneResult = 0;
 
     public void process() {
-        for (String code: codes) {
+        Arrays.fill(keypadPositions, 'A');
+
+        for (String code : codes) {
             // String mapLevelOne = mapToNumeric(code); // First robot
             // String mapLevelTwo = mapToDirectional(mapLevelOne, 1); // Second robot
             // String result = mapToDirectional(mapLevelTwo, 2); // Me controlling third robot
@@ -247,49 +295,70 @@ public class Day21 extends PuzzleApp {
             // partOneResult += (result.length() * Integer.parseInt(code.substring(0,3)));
 
             Set<String> s1 = mapToNumericAll(code);
-            // s1.forEach(x -> System.out.println(x + " -> " + testNumeric(x)));
-            Set<String> s2 = s1.stream().flatMap(s -> mapToDirectionalAll(s, 1).stream()).collect(Collectors.toSet());
-            // s2.forEach(x -> System.out.println(x + " -> " + testNumeric(testDirectional(x))));
-            Set<String> s3 = s2.stream().flatMap(s -> mapToDirectionalAll(s, 2).stream()).collect(Collectors.toSet());
-            // s3.forEach(x -> System.out.println(x + " -> " + testNumeric(testDirectional(testDirectional(x)))));
+            int minLength = s1.stream().mapToInt(String::length).min().orElse(0);
+            int finalMinLength1 = minLength;
+            s1 = s1.stream().filter(s -> s.length() == finalMinLength1).collect(Collectors.toSet());
+            System.out.println("Code " + code + " iteration " + 0 + " complete, set size = " + s1.size());
 
-            int minLength = s3.stream().mapToInt(String::length).min().orElse(0);
-            int codeValue = Integer.parseInt(code.substring(0,3));
+            for (int i = 1; i <= 2; i++) {
+                int finalI = i;
+                s1 = s1.stream().flatMap(s -> mapToDirectionalAll(s, finalI).stream()).collect(Collectors.toSet());
+                minLength = s1.stream().mapToInt(String::length).min().orElse(0);
+                int finalMinLength2 = minLength;
+                s1 = s1.stream().filter(s -> s.length() == finalMinLength2).collect(Collectors.toSet());
+                System.out.println("Code " + code + " iteration " + i + " complete, set size = " + s1.size());
+            }
+
+            minLength = s1.stream().mapToInt(String::length).min().orElse(0);
+            int codeValue = Integer.parseInt(code.substring(0, 3));
             partOneResult += (minLength * codeValue);
             System.out.println("Code " + code + ": " + minLength + " * " + codeValue + " = " + (minLength * codeValue));
-         }
+        }
     }
 
     public void results() {
         System.out.println("Day 21 part 1 result: " + partOneResult);
     }
 
-    private int partTwoResult = 0;
+    private final Map<String,Long> shortestPathForEachCharacter = new HashMap<>();
 
     public void processPartTwo() {
-        for (String code: codes) {
-            Set<String> s1 = mapToNumericAll(code);
-            int minLength = s1.stream().mapToInt(String::length).min().orElse(0);
-            int finalMinLength = minLength;
-            s1 = s1.stream().filter(s -> s.length() == finalMinLength).collect(Collectors.toSet());
+        Arrays.fill(keypadPositions, 'A');
 
-            for (int i = 0; i < 25; i++) {
+        String[] keypadCharacters = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A"};
+
+        for (String code : keypadCharacters) {
+            Set<String> s1 = mapToNumericAll(code);
+            long minLength = s1.stream().mapToLong(String::length).min().orElse(0);
+            long finalMinLength = minLength;
+            s1 = s1.stream().filter(s -> s.length() == finalMinLength).collect(Collectors.toSet());
+            System.out.println("Code " + code + " iteration " + 0 + " complete, set size = " + s1.size());
+
+            for (int i = 1; i <= 25; i++) {
                 int finalI = i;
                 s1 = s1.stream().flatMap(s -> mapToDirectionalAll(s, finalI).stream()).collect(Collectors.toSet());
-                minLength = s1.stream().mapToInt(String::length).min().orElse(0);
-                int finalMinLength1 = minLength;
+                minLength = s1.stream().mapToLong(String::length).min().orElse(0);
+                long finalMinLength1 = minLength;
                 s1 = s1.stream().filter(s -> s.length() == finalMinLength1).collect(Collectors.toSet());
                 System.out.println("Code " + code + " iteration " + i + " complete, set size = " + s1.size());
             }
 
-            minLength = s1.stream().mapToInt(String::length).min().orElse(0);
-            int codeValue = Integer.parseInt(code.substring(0,3));
-            partTwoResult += (minLength * codeValue);
-            System.out.println("Code " + code + ": " + minLength + " * " + codeValue + " = " + (minLength * codeValue));
+            minLength = s1.stream().mapToLong(String::length).min().orElse(0);
+            shortestPathForEachCharacter.put(code, minLength);
+            System.out.println("Code " + code + ": " + minLength);
         }
     }
 
     public void resultsPartTwo() {
+        long partTwoResult = 0;
+
+        for (String code : codes) {
+            long codeValue = Long.parseLong(code.substring(1,3));
+            long codeLength = code.chars().mapToLong(c -> shortestPathForEachCharacter.get(String.valueOf((char)c))).sum();
+            System.out.println("Code " + code + " has total length " + codeLength);
+            partTwoResult += (codeLength * codeValue);
+        }
+
         System.out.println("Day 21 part 2 result: " + partTwoResult);
     }
 }
